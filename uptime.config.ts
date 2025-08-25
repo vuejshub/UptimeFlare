93c4182b700cb1c3fba44219cc4eb40565b56958
vuejshub <58390198+vuejshub@users.noreply.github.com>
2025-08-25 11:44:31 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -2,12 +2,11 @@ import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
 const pageConfig: PageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://blog.lyc8503.net/', label: 'Blog' }
   ],
   // [OPTIONAL] Group your monitors
   // If not specified, all monitors will be shown in a single list
